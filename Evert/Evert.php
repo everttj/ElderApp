1ac18a2f0aae240f333ee137aad855908328d522
KevinLong021 <Kevin.long.021@gmail.com>
2019-03-10 22:26:29 -0400

    Update repo to reflect full contents of public_html

---
+++ Evert/Evert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mr. Evert's Page</title>
+	</head>
+
+	<body>
+		<h1>Mr. Evert's Page</h1>
+
+		<p>This  is a <b>webpage</b>. There are many like it, but this one is <i>mine</i>.</p>
+
+		<?php
+			echo "This is PHP!";
+		?>
+		
+		<script>
+		for(var i = 1; i <= 10; i++)
+		  document.write("Hello!");
+		</script>
+	</body>
+
+</html>
---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+    document.getElementById("mrevert").addEventListener('click', function(){
+        window.open('/Evert/Evert.php');
+    });
+
+    document.getElementById("jacob").addEventListener('click', function(){
+        window.open('/Fullenkamp/Fullenkamp.html');
+    });
+
+    document.getElementById("jared").addEventListener('click', function(){
+        window.open('/Keith/Keith.html');
+    });
+
+    document.getElementById("kevin").addEventListener('click', function(){
+        window.open('/long');
+    });
+
+    document.getElementById("josh").addEventListener('click', function(){
+        window.open('/Martini/Martini.html');
+    });
+
+    document.getElementById("brian").addEventListener('click', function(){
+        window.open('/Miller/Miller.html');
+    });
+}
